0d77522ea786b45170b796451c28c85f61846f7d
mrhrifat <mrhrifat383759@gmail.com>
2021-12-16 05:08:41 +0600

    Create card for view weather message

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,10 +4,13 @@ import '../App.css'
 
 const Root = () => {
     const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState({})
     const search = async (e) => {
         if (e.key === 'Enter') {
             const data = await getWeather(query)
-            console.log(data)
+            // console.log(data)
+            setWeather(data)
+            setQuery('')
         }
     }
     return (
@@ -20,6 +23,23 @@ const Root = () => {
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyPress={search}
             />
+            {weather.main && (
+                <div className='city'>
+                    <h2 className='city-name'>
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg; C</sup>
+                    </div>
+                    <div className="info">
+                        <img className='city-icon' src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+                        alt={weather.weather[0].description}/>
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
