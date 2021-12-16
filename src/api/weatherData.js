d18448df4b4b1aedd3f51af8ecc48a74582125e1
mrhrifat <mrhrifat383759@gmail.com>
2021-12-16 12:19:43 +0600

    Add API url to Env

--- src/api/weatherData.js
+++ src/api/weatherData.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const URL = process.env.REACT_APP_API_URL;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const getWeather = async (query) => {
